667acc82ccfbfdfebad5cbbbc084e2a4bb1a99ce
chai-kat <17334965+chai-kat@users.noreply.github.com>
2024-06-02 13:47:34 +0200

    insertion sort with generated array

---
+++ tests/test-ocamlInsertionSort.ml
@@ -0,0 +1,32 @@
+let insertionSort array =
+    match ((List.length s) < 2) with true then 
+        array
+    else
+        let rec insert_sorted element sorted_arr
+            match sorted_arr with [] then
+                [element]
+            else
+                match (leqi  element (get sorted_arr 0)) with true then
+                    cons element sorted_arr
+                else
+                    cons (get sorted_arr 0) (insert_sorted element (subsequence sorted_arr 1 (length sorted_arr)))
+        in
+
+        recursive let sort_recursively = lam unsorted_arr. lam sorted_arr.
+            match unsorted_arr with [] then
+                sorted_arr
+            else
+                sort_recursively (subsequence unsorted_arr 1 (length unsorted_arr)) (insert_sorted (get unsorted_arr 0) sorted_arr)
+        in
+
+        sort_recursively (subsequence array 1 (length array)) [(get array 0)]
+in
+
+recursive let fill_sequence = lam s. lam pos. lam len. 
+    match (eqi len 0) with true then s
+    else
+        (fill_sequence (cons pos s) (addi pos 1)) (subi len 1)
+in let sequence = fill_sequence [] 0 10000000
+in
+(sequence)
+-- (insertionSort sequence)
